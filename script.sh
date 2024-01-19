0d9a79510cd63d5d4455c78cb2a86d5746296999
Lucas Simon <lucasimonmendes@hotmail.com>
2024-01-19 14:31:17 -0300

    :sparkles: add functions

--- script.sh
+++ script.sh
@@ -8,23 +8,55 @@ URL_SIMPLENOTE="https://github.com/Automattic/simplenote-electron/releases/downl
 
 PATH_DOWNLOADS_PROGRAMS="$HOME/Downloads/programas"
 
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/cache/apt/archives/lock
-sudo dpkg --add-architecture i386
-sudo apt update -y
-sudo apt-add-repository "$PPA_PIPER_LIBRATG" -y
-sudo add-apt-repository "$PPA_LUTRIS" -y
-sudo apt update -y
-mkdir "$PATH_DOWNLOADS_PROGRAMS"
-wget "$URL_GOOGLE_CHROME" -P "$PATH_DOWNLOADS_PROGRAMS"
-wget "$URL_SIMPLENOTE" -P    "$PATH_DOWNLOADS_PROGRAMS"
-sudo dpkg -i /home/lucas/Downloads/programas/*.deb
-sudo apt -f install -y
-sudo apt install snapd -y
-sudo apt install winff -y
-sudo apt install guvcview -y
-sudo apt install virtualbox -y
-sudo snap install spotify
-sudo apt update && sudo apt dist-upgrade -y
-sudo apt autoclean
-sudo apt autoremove -y
+
+remove_locks () {
+  sudo rm /var/lib/dpkg/lock-frontend
+  sudo rm /var/cache/apt/archives/lock
+}
+
+add_i386_architecture () {
+ sudo dpkg --add-architecture i386 
+}
+
+update_repositories () {
+  sudo apt update -y
+}
+
+add_ppas () {
+  sudo apt-add-repository "$PPA_PIPER_LIBRATG" -y
+  sudo add-apt-repository "$PPA_LUTRIS" -y
+  update_repositories
+}
+
+install_debs () {
+  mkdir "$PATH_DOWNLOADS_PROGRAMS"
+  wget "$URL_GOOGLE_CHROME" -P "$PATH_DOWNLOADS_PROGRAMS"
+  wget "$URL_SIMPLENOTE" -P    "$PATH_DOWNLOADS_PROGRAMS"
+  sudo dpkg -i $PATH_DOWNLOADS_PROGRAMS/*.deb
+  sudo apt -f install -y
+}
+
+install_apt_packages () {
+  sudo apt install snapd -y
+  sudo apt install winff -y
+  sudo apt install guvcview -y
+  sudo apt install virtualbox -y
+}
+
+install_snap_packages() {
+  sudo snap install spotify
+}
+
+upgrade_and_clear_system () {
+  sudo apt dist-upgrade -y
+  sudo apt autoclean
+  sudo apt autoremove -y
+}
+
+
+remove_locks
+add_i386_architecture
+add_ppas
+install_apt_packages
+install_snap_packages
+upgrade_and_clear_system
