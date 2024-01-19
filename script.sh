8ade7681197713b88c462404ffe07d8ddadc2af9
Lucas Simon <lucasimonmendes@hotmail.com>
2024-01-19 11:10:50 -0300

    :sparkles: add variables

--- script.sh
+++ script.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+PPA_PIPER_LIBRATG="ppa:libratbag-piper/piper-libratbag-git "
+PPA_LUTRIS="ppa:lutris-team/lutris"
+
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+URL_SIMPLENOTE="https://github.com/Automattic/simplenote-electron/releases/download/v1.8.0/Simplenote-linux-1.8.0-amd64.deb"
+
+PATH_DOWNLOADS_PROGRAMS="$HOME/Downloads/programas"
+
 sudo rm /var/lib/dpkg/lock-frontend
 sudo rm /var/cache/apt/archives/lock
 sudo dpkg --add-architecture i386
 sudo apt update -y
-sudo apt-add-repository ppa:libratbag-piper/piper-libratbag-git -y
-sudo add-apt-repository ppa:lutris-team/lutris -y
+sudo apt-add-repository "$PPA_PIPER_LIBRATG" -y
+sudo add-apt-repository "$PPA_LUTRIS" -y
 sudo apt update -y
-mkdir /home/lucas/Downloads/programas
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /home/lucas/Downloads/programas
-wget https://github.com/Automattic/simplenote-electron/releases/download/v1.8.0/Simplenote-linux-1.8.0-amd64.deb -P /home/lucas/Downloads/programas
+mkdir "$PATH_DOWNLOADS_PROGRAMS"
+wget "$URL_GOOGLE_CHROME" -P "$PATH_DOWNLOADS_PROGRAMS"
+wget "$URL_SIMPLENOTE" -P    "$PATH_DOWNLOADS_PROGRAMS"
 sudo dpkg -i /home/lucas/Downloads/programas/*.deb
 sudo apt -f install -y
 sudo apt install snapd -y
